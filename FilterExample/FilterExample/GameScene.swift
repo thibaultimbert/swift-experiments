c9dc5d28be95c1373dfaa2173f9aeb293964e090
Thibault Imbert <timbert@adobe.com>
2014-06-20 15:10:39 -0700

    Changed some vars to consts (let)

--- FilterExample/FilterExample/GameScene.swift
+++ FilterExample/FilterExample/GameScene.swift
@@ -1,11 +1,3 @@
-//
-//  GameScene.swift
-//  FilterExample
-//
-//  Created by Thibault Imbert on 2014-06-11.
-//  Copyright (c) 2014 Thibault Imbert. All rights reserved.
-//
-
 import SpriteKit
 
 class GameScene: SKScene {
@@ -16,7 +8,7 @@ class GameScene: SKScene {
         UIGraphicsBeginImageContextWithOptions(CGSize(width: 200, height: 200), true, 1)
         
         // we retrieve it
-        var context = UIGraphicsGetCurrentContext()
+        let context = UIGraphicsGetCurrentContext()
         
         // we issue drawing commands
         CGContextSetRGBFillColor (context, 1, 1, 0, 1);
@@ -29,29 +21,29 @@ class GameScene: SKScene {
         CGContextFillRect (context, CGRectMake (0, 0, 50, 100));
         
         // we query an image from it
-        var image = UIGraphicsGetImageFromCurrentImageContext()
+        let image = UIGraphicsGetImageFromCurrentImageContext()
         
         // we create Core Image context
-        var ciContext = CIContext(options: nil)
+        let ciContext = CIContext(options: nil)
         // we create a CIImage, think of a CIImage as image data for processing, nothing is displayed or can be displayed at this point
-        var coreImage = CIImage(image: image)
+        let coreImage = CIImage(image: image)
         // we pick the filter we want
-        var filter = CIFilter(name: "CIGaussianBlur")
+        let filter = CIFilter(name: "CIGaussianBlur")
         // we pass our image as input
         filter.setValue(coreImage, forKey: kCIInputImageKey)
         // we retrieve the processed image
-        var filteredImageData = filter.valueForKey(kCIOutputImageKey) as CIImage
+        let filteredImageData = filter.valueForKey(kCIOutputImageKey) as CIImage
         // returns a Quartz image from the Core Image context
-        var filteredImageRef = ciContext.createCGImage(filteredImageData, fromRect: filteredImageData.extent())
+        let filteredImageRef = ciContext.createCGImage(filteredImageData, fromRect: filteredImageData.extent())
         // this is our final UIImage ready to be displayed
-        var filteredImage = UIImage(CGImage: filteredImageRef);
+        let filteredImage = UIImage(CGImage: filteredImageRef);
         
         // we create a texture, pass the UIImage
-        var texture = SKTexture(image: filteredImage)
+        let texture = SKTexture(image: filteredImage)
         // wrap it inside a sprite node
-        var sprite = SKSpriteNode(texture:texture)
+        let sprite = SKSpriteNode(texture:texture)
         // we scale it a bit
-        //sprite.setScale(0.5);
+        sprite.setScale(0.5);
         // we position it
         sprite.position = CGPoint (x: 510, y: 380)
         // let's display it
