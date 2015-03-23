ec41990c472c88e7dd50f1d74d943bda7023d709
Thibault Imbert <timbert@adobe.com>
2015-03-22 22:20:27 -0700

    Updated projects to Xcode 6.2

---
+++ CGDemo/CGDemo/CustomButton.swift
@@ -0,0 +1,9 @@
+//
+//  CustomButton.swift
+//  CGDemo
+//
+//  Created by Thibault Imbert on 2015-03-22.
+//  Copyright (c) 2015 Thibault Imbert. All rights reserved.
+//
+
+import Foundation
---
+++ CGDemo/CGDemoTests/CGDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  CGDemoTests.swift
+//  CGDemoTests
+//
+//  Created by Thibault Imbert on 2015-03-22.
+//  Copyright (c) 2015 Thibault Imbert. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CGDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
